5b0b6eb79254beed30e40f2491eb4d25c353eede
Filippo Valsorda <filippo@cloudflare.com>
2016-08-31 14:12:59 +0100

    Fix a stupid buffer reuse bug (found it!)

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/aes"
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -31,14 +32,22 @@ func main() {
 	}
 
 	scmKey := data[keyOffset : keyOffset+keyLen]
+	scrKey := make([]byte, len(scmKey))
+
 	salt := data[saltOffset : saltOffset+saltLen]
+	log.Println("Salt:")
+	fmt.Fprint(os.Stderr, hex.Dump(salt))
+
 	check := data[checkOffset : checkOffset+checkLen]
+	log.Println("Checksum:")
+	fmt.Fprint(os.Stderr, hex.Dump(check))
 
 	var rounds uint32
 	if err := binary.Read(bytes.NewReader(data[roundsOffset:]),
 		binary.LittleEndian, &rounds); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Rounds:", rounds)
 
 	try := func(pass string) bool {
 		maskkey := pbkdf2.Key([]byte(pass), salt, int(rounds), 32, sha1.New)
@@ -49,35 +58,34 @@ func main() {
 			log.Fatal(err)
 		}
 		for i := 0; i < len(scmKey); i += a.BlockSize() {
-			a.Decrypt(scmKey[i:i+a.BlockSize()], scmKey[i:i+a.BlockSize()])
+			a.Decrypt(scrKey[i:i+a.BlockSize()], scmKey[i:i+a.BlockSize()])
 		}
 
 		// HMAC-SHA1(k=maskkey, scm_key) == sch_mac
 		h := sha1.Sum(maskkey)
 		mac := hmac.New(sha1.New, h[:])
-		mac.Write(scmKey)
+		mac.Write(scrKey)
 		expected := mac.Sum(nil)
 
 		return bytes.Equal(expected, check)
 	}
 
-	f, err := os.Open("wordlist.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(f)
-	i := 0
-	for scanner.Scan() {
-		if try(scanner.Text()) {
-			log.Print(scanner.Text())
-			return
+	log.Println("Starting...")
+
+	word1 := []string{}
+	word2 := []string{}
+	word3 := []string{}
+	word4 := []string{}
+
+	for _, w1 := range word1 {
+		for _, w2 := range word2 {
+			for _, w3 := range word3 {
+				for _, w4 := range word4 {
+					if try(w1 + w2 + w3 + w4) {
+						log.Fatalln("YES, YES, YES!", w1+w2+w3+w4)
+					}
+				}
+			}
 		}
-		i++
-		if i%256 == 0 {
-			log.Print(i)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
 	}
 }
